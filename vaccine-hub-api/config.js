361183c8b3cec34b1401c74cc0e36708fa2afc54
doowee0509 <ngocduy000937@gmail.com>
2022-06-28 13:18:53 -0400

    finished all core features

--- vaccine-hub-api/config.js
+++ vaccine-hub-api/config.js
@@ -13,6 +13,7 @@ function getDatabaseUri () {
     return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
 }
 
+const BCRYPT_WORK_FACTOR = 13
 // console.log("process.env".yellow, Object.keys(process.env))
 console.log("App Config".red)
 console.log("PORT:".blue, PORT)
@@ -21,5 +22,6 @@ console.log("---")
 
 module.exports = {
     PORT,
+    BCRYPT_WORK_FACTOR,
     getDatabaseUri
 }
--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -1,11 +1,35 @@
 const db = require("../db")
 const bcrypt = require("bcrypt")
+const {BCRYPT_WORK_FACTOR} = require("../config")
 const { BadRequestError, UnauthorizedError } = require("../utils/errors")
 class User {
+    static async makePublicUser(user) {
+        return {
+            id: user.id,
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            location: user.location,
+            date: user.date
+        }
+    }
+
     static async login(creds) {
         //required fields are email and password, throw error if either are missing
-
+        const requiredFields = ["email", 'password']
+        requiredFields.forEach(field => {
+            if (!creds.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`)
+            }
+        })
         //looking the user in the db by email
+        const user = await User.fetchUserByEmail(creds.email)
+        if (user) {
+            const isValid = await bcrypt.compare(creds.password, user.password)
+            if (isValid) {
+                return User.makePublicUser(user)
+            }
+        }
         // if the user is found, compare the password with the submitted password
         // if they match, return user
         // else throw an error
@@ -33,6 +57,8 @@ class User {
         }
 
         // hash user password
+        
+        const hashedPassword = await bcrypt.hash(creds.password, BCRYPT_WORK_FACTOR)
         // lowercase email
         const lowercasedEmail = creds.email.toLowerCase()
         //create new user in the db with given info
@@ -47,10 +73,10 @@ class User {
             VALUES ($1, $2, $3, $4, $5)
             RETURNING id, email, password, first_name, last_name, location, date;
         `,
-        [lowercasedEmail, creds.password, creds.firstName, creds.lastName, creds.location]
+        [lowercasedEmail, hashedPassword, creds.firstName, creds.lastName, creds.location]
         )
         //return user
-        return result.rows[0]
+        return User.makePublicUser(result.rows[0])
     }
 
     static async fetchUserByEmail(email) {
